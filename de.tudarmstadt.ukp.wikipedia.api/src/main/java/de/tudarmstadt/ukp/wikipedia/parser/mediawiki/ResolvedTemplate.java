5e977f2d909e8d569dcd9c1d7582abbcb52817cb
Torsten Zesch <torsten.zesch@gmail.com>
2010-09-21 09:27:09 +0000

    Initial code drop.

---
+++ de.tudarmstadt.ukp.wikipedia.api/src/main/java/de/tudarmstadt/ukp/wikipedia/parser/mediawiki/ResolvedTemplate.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Torsten Zesch.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Torsten Zesch - initial API and implementation
+ ******************************************************************************/
+package de.tudarmstadt.ukp.wikipedia.parser.mediawiki;
+
+import de.tudarmstadt.ukp.wikipedia.parser.Template;
+
+class ResolvedTemplate{
+	protected final static String templateSpacer = "(TEMPLATE)";
+	
+	private Template template;
+	private String preParseReplacement;
+	private String postParseReplacement;
+	
+	/**
+	 * is the Object wich the Template Parser has been parsed, and will be integrated
+	 * by the ContentElementParseing process. It parsedObject == null, the template will 
+	 * be thrown away...
+	 */
+	private Object parsedObject;
+	
+	/**
+	 * Creates a new ResolvedTemplate linked to the original template.
+	 * @param template the original template
+	 */
+	public ResolvedTemplate(Template template){
+		this.template = template;
+		this.postParseReplacement = "";
+		checkPreParseReplacement();		
+	}
+
+	private void checkPreParseReplacement(){
+		if( preParseReplacement==null || preParseReplacement.length()==0 ) preParseReplacement = templateSpacer;
+	}
+	
+	/**
+	 * Will be called by the parser after the parseing process and will replace the TEXT which
+	 * is in the bounds of the original template src. <br/>
+	 * If NULL is returned, the parser wouldn�t do anything.
+	 */
+	public String getPostParseReplacement() {
+		return postParseReplacement;
+	}
+
+	/**
+	 * Look at getPostParseReplacement...
+	 */
+	public void setPostParseReplacement(String postParseReplacement) {
+		this.postParseReplacement = postParseReplacement;
+	}
+
+	/**
+	 * will be called by the parser before the Parsing process and replaces the original 
+	 * template code. MediaWiki code which is returned here, will be parsed.<br/>
+	 * length()>0 ! empty stings would not be accepted.
+	 */
+	public String getPreParseReplacement() {
+		return preParseReplacement;
+	}
+
+	/**
+	 * Look at getPreParseReplacement...
+	 */
+	public void setPreParseReplacement(String preParseReplacement) {
+		this.preParseReplacement = preParseReplacement;
+		checkPreParseReplacement();
+	}
+
+	/**
+	 * In case of an Error the Parser will use the Original Template
+	 * as parsed object.
+	 */
+	public Template getTemplate() {
+		return template;
+	}
+
+	/**
+	 * Retruns the Object which is representativ for the Template Code.
+	 * It can be a Template or any object the parser knows.<br/>
+	 * If the Template is e.g. a Link the Link will be returned here.
+	 */
+	public Object getParsedObject() {
+		return parsedObject;
+	}
+
+	/**
+	 * Look at getParsedObject for Details.
+	 * @param parsedObject
+	 */
+	public void setParsedObject(Object parsedObject) {
+		this.parsedObject = parsedObject;
+	}	
+}
---
+++ de.tudarmstadt.ukp.wikipedia.api/src/test/java/de/tudarmstadt/ukp/wikipedia/api/CategoryDescendantsIteratorTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Torsten Zesch.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Torsten Zesch - initial API and implementation
+ ******************************************************************************/
+package de.tudarmstadt.ukp.wikipedia.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.wikipedia.api.WikiConstants.Language;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiApiException;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiInitializationException;
+
+public class CategoryDescendantsIteratorTest {
+
+	private Wikipedia wiki;
+	
+	@Before
+	public void setupWikipedia() {
+		DatabaseConfiguration db = new DatabaseConfiguration();
+		db.setDatabase("wikiapi_test");
+		db.setHost("bender.tk.informatik.tu-darmstadt.de");
+		db.setUser("student");
+		db.setPassword("student");
+		db.setLanguage(Language._test);
+		
+		try {
+			wiki = new Wikipedia(db);
+		} catch (WikiInitializationException e) {
+			fail("Wikipedia could not be initialized.");
+		}
+	}
+	
+
+	/**
+	 * The category UKP has 9 descendants with pageIds 7-15.
+	 */
+	@Test
+	public void test_categoryIteratorTest() {
+		
+        Category cat = null;
+        try {
+            cat = wiki.getCategory("UKP");
+        } catch (WikiApiException e) {
+            e.printStackTrace();
+            fail("A WikiApiException occured while getting the category 'UKP'");
+        }
+
+
+        List<Integer> expectedPageIds = new ArrayList<Integer>();
+        expectedPageIds.add(7);
+        expectedPageIds.add(8);
+        expectedPageIds.add(9);
+        expectedPageIds.add(10);
+        expectedPageIds.add(11);
+        expectedPageIds.add(12);
+        expectedPageIds.add(13);
+        expectedPageIds.add(14);
+        expectedPageIds.add(15);
+
+        List<Integer> isIds = new ArrayList<Integer>();
+        for(Category descendant : cat.getDescendants()) {
+            isIds.add(descendant.getPageId());
+        }
+        Collections.sort(expectedPageIds);
+        Collections.sort(isIds);
+        assertEquals("descendants", expectedPageIds, isIds);
+	}
+	
+    /**
+     * The category UKP has 9 descendants with pageIds 7-15.
+     */
+	@Test
+	public void test_categoryIteratorTestBufferSize() {
+		
+        Category cat = null;
+        try {
+            cat = wiki.getCategory("UKP");
+        } catch (WikiApiException e) {
+            e.printStackTrace();
+            fail("A WikiApiException occured while getting the category 'UKP'");
+        }
+
+
+        List<Integer> expectedPageIds = new ArrayList<Integer>();
+        expectedPageIds.add(7);
+        expectedPageIds.add(8);
+        expectedPageIds.add(9);
+        expectedPageIds.add(10);
+        expectedPageIds.add(11);
+        expectedPageIds.add(12);
+        expectedPageIds.add(13);
+        expectedPageIds.add(14);
+        expectedPageIds.add(15);
+
+		for (int bufferSize=1;bufferSize<=100;bufferSize+=5) {
+            List<Integer> isIds = new ArrayList<Integer>();
+            for(Category descendant : cat.getDescendants(bufferSize)) {
+                isIds.add(descendant.getPageId());
+            }
+            Collections.sort(expectedPageIds);
+            Collections.sort(isIds);
+            assertEquals("descendants", expectedPageIds, isIds);
+
+        }
+	}
+}
