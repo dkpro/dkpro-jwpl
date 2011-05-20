4876080044b77226f345cd78eb893ca46e3f98e6
oliver.ferschke <oliver.ferschke@10d8a5b4-7c7b-defa-3ec6-669fcde35942>
2011-05-20 14:24:00 +0000

    Fixed compile warnings in RevisionMachine

--- trunk/de.tudarmstadt.ukp.wikipedia.revisionmachine/src/main/java/de/tudarmstadt/ukp/wikipedia/revisionmachine/difftool/producer/articles/ArticleManager.java
+++ trunk/de.tudarmstadt.ukp.wikipedia.revisionmachine/src/main/java/de/tudarmstadt/ukp/wikipedia/revisionmachine/difftool/producer/articles/ArticleManager.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (c) 2011 Ubiquitous Knowledge Processing Lab
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl.html
- * 
+ *
  * Project Website:
  * 	http://jwpl.googlecode.com
- * 
+ *
  * Contributors:
  * 	Torsten Zesch
  * 	Simon Kulessa
@@ -88,7 +88,7 @@ public class ArticleManager
 
 		this.logger = articleProducer.getLogger();
 
-		ConfigurationManager config = ConfigurationManager.getInstance();
+		ConfigurationManager.getInstance();
 
 		this.articleProducer = articleProducer;
 
