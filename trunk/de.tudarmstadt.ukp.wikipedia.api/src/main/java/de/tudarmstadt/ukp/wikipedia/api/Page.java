b61ccc658a7090576538b55143b1501bec2b13e8
oliver.ferschke <oliver.ferschke@10d8a5b4-7c7b-defa-3ec6-669fcde35942>
2011-08-20 20:33:41 +0000

    readding colation to query (due to failing unit tests)

--- trunk/de.tudarmstadt.ukp.wikipedia.api/src/main/java/de/tudarmstadt/ukp/wikipedia/api/Page.java
+++ trunk/de.tudarmstadt.ukp.wikipedia.api/src/main/java/de/tudarmstadt/ukp/wikipedia/api/Page.java
@@ -155,7 +155,7 @@ public class Page
 		session.beginTransaction();
 		Integer pageId = (Integer) session
 				.createSQLQuery(
-						"select pml.pageID from PageMapLine as pml where pml.name = ? LIMIT 1")
+						"select pml.pageID from PageMapLine as pml where pml.name = ? COLLATE utf8_bin")
 				.setString(0, searchString).uniqueResult();
 
 		if (pageId == null) {
