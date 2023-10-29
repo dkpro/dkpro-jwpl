1bd54370b3cbc8b83a010e0c6757268eff7fe40c
Martin Wiesner <mawiesne@users.noreply.github.com>
2023-10-29 21:11:45 +0100

    #247 Update dependency commons-lang to version 3.x (#248)
    - gets rid of the old commons-lang 2.x dependency
    - updates / manages commons-lang3 to version 3.13.0

--- dkpro-jwpl-util/src/main/java/org/dkpro/jwpl/util/templates/TextPair.java
+++ dkpro-jwpl-util/src/main/java/org/dkpro/jwpl/util/templates/TextPair.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import difflib.Delta;
 import difflib.Delta.TYPE;
